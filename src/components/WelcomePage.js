1dade63db1f762376987314fca42da846201a681
Erkin Aydın <erkinresearch@gmail.com>
2023-10-12 22:36:35 +0300

    welcome message changed.

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -14,30 +14,15 @@ const WelcomePage = () => {
           }}
         >
         <h1>Project PrioVar</h1>
-        <p>Introducing 'PrioVar,' where science meets precision to transform rare disease diagnosis. 
-            We leverage cutting-edge analysis of phenotype and genotype data to prioritize variants, 
-            providing medical professionals with actionable insights. Our mission is simple: empower early 
-            and accurate diagnosis, ultimately improving the lives of those affected by rare diseases.</p>
-        <br/>
-        <h2>Details of PrioVar</h2>
-        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, 
-            eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam 
-            voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione 
-            voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-            sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad 
-            minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi 
-            consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, 
-            vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
-        <p>
-        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer suscipit id purus nec 
-        semper. Mauris scelerisque, turpis vel tempor accumsan, justo diam venenatis lacus, in vehicula turpis ex congue magna. 
-        Suspendisse id finibus ipsum. Morbi fermentum velit ex, sed sagittis lorem consectetur vitae. Curabitur mi enim, fringilla 
-        a nibh et, volutpat tincidunt erat. Pellentesque sit amet purus ut metus luctus laoreet. Suspendisse nunc eros, facilisis 
-        sed condimentum ac, suscipit vitae nibh. Nullam rutrum, ex et consectetur imperdiet, justo nibh vehicula ex, a euismod dui 
-        tellus quis ante. Cras euismod, odio quis imperdiet finibus, libero augue porta nunc, in tempor enim lacus viverra magna. 
-        Nunc eu velit a nulla porttitor mollis vel sed magna. Phasellus aliquet efficitur turpis vitae tincidunt. Aliquam vitae 
-        ultrices metus. Sed tincidunt risus et risus ultrices aliquet. Fusce feugiat vel lacus rutrum euismod. 
-        </p>
+        <p>We introduce 'PrioVar,' where bioinformatics meet precision to transform rare disease diagnosis. 
+            Our project is a cutting-edge genetic variant prioritization tool tailored for health clinics, 
+            designed to seamlessly integrate phenotype data into the decision-making process. 
+            Clinics can prioritize genetic variants in order to make more accurate predictions about the 
+            causes behind rare diseases, exchange valuable variant frequency information via our interface, 
+            and gain access to anonymous data from individuals with similar phenotypes/genotypes with known 
+            diseases, all of which collectively revolutionize the detection and understanding of genetic 
+            conditions. Our mission is simple: empower early and accurate diagnosis, ultimately improving the 
+            lives of those affected by rare diseases.</p>
         </div>
     )
 };
