fcfbcc04c29ef6de2719ffc678e3ca6f63770e11
HP <alperengozeten@gmail.com>
2023-11-08 19:02:23 +0300

    backend init

---
+++ backend/priovar/src/main/java/com/bio/priovar/models/Patient.java
@@ -0,0 +1,20 @@
+package com.bio.priovar.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Table
+@Entity
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ID;
+    @OneToOne
+    private MedicalCenter medicalCenter;
+}
