13d096528c7319cb50e62f16a1d4b485a7cb10d5
safa55 <eren.kuday@ug.bilkent.edu.tr>
2023-11-26 00:28:49 +0300

    fix: dependency correction

--- backend/priovar/src/main/java/com/bio/priovar/repositories/PatientRepository.java
+++ backend/priovar/src/main/java/com/bio/priovar/repositories/PatientRepository.java
@@ -1,8 +1,8 @@
 package com.bio.priovar.repositories;
 
 import com.bio.priovar.models.Patient;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
