b6b850b79f4d65f8df63c4a9aa0a987cb5fb2234
HP <alperengozeten@gmail.com>
2023-12-17 22:27:57 +0300

    correct path

--- frontend_cs491_org/src/routes/paths.js
+++ frontend_cs491_org/src/routes/paths.js
@@ -7,7 +7,7 @@ function path(root, sublink) {
 const ROOTS_AUTH = '/auth'
 const ROOTS_DASHBOARD = '/libra'
 const ROOTS_SAMPLE = '/sample'
-const ROOTS_PRIOVAR = 'localhost:8080'
+export const ROOTS_PRIOVAR = 'http://localhost:8080'
 // ----------------------------------------------------------------------
 
 export const PATH_PRIOVAR = {
