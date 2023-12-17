d7b0935c45178899a05a3e89d776ec41a9d5545e
HP <alperengozeten@gmail.com>
2023-12-17 17:49:42 +0300

    feat: add patient with phenotypes

---
+++ backend/priovar/src/main/java/com/bio/priovar/models/dto/PatientWithPhenotype.java
@@ -0,0 +1,17 @@
+package com.bio.priovar.models.dto;
+
+import com.bio.priovar.models.Patient;
+import com.bio.priovar.models.PhenotypeTerm;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PatientWithPhenotype {
+    private Patient patient;
+    private List<PhenotypeTerm> phenotypeTerms;
+}
