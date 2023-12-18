4a80dbfebd396ada10ee656a5266c3341cf512d1
Korhan Kemal Kaya <79211946+korhankemalkaya@users.noreply.github.com>
2023-12-18 19:34:42 +0300

    feat: Custom Query 90% completed.

---
+++ backend/priovar/src/main/java/com/bio/priovar/controllers/QueryController.java
@@ -0,0 +1,27 @@
+package com.bio.priovar.controllers;
+import com.bio.priovar.models.Patient;
+import com.bio.priovar.services.QueryService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bio.priovar.models.Query;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class QueryController {
+
+    private final QueryService queryService;
+
+    @Autowired
+    public QueryController(QueryService queryService) {
+        this.queryService = queryService;
+    }
+
+    @GetMapping("/customQuery")
+    public List<Patient> runCustomQuery(@RequestBody Query query) {
+        return queryService.runCustomQuery(query);
+    }
+
+}
