ff243eed03267491bcaf143ee534ebcde2b80e51
HP <alperengozeten@gmail.com>
2023-12-20 01:11:02 +0300

    feat: get clinician by medical center

--- backend/priovar/src/main/java/com/bio/priovar/repositories/ClinicianRepository.java
+++ backend/priovar/src/main/java/com/bio/priovar/repositories/ClinicianRepository.java
@@ -4,7 +4,11 @@ import com.bio.priovar.models.Clinician;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ClinicianRepository extends Neo4jRepository<Clinician, Long> {
     Clinician findByEmail(String email);
+
+    List<Clinician> findAllByMedicalCenterId(Long medicalCenterId);
 }
