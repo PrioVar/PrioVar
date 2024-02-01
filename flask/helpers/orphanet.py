7db43eab6b203664a94180142c4600a0e9daf307
Erkin Aydın <erkinresearch@gmail.com>
2024-02-01 23:56:31 +0300

    feat: printing the number of relations and relations themselves in orphanet.

--- flask/helpers/orphanet.py
+++ flask/helpers/orphanet.py
@@ -36,5 +36,40 @@ for disorder in disease_to_gene.find_all('Disorder'):
     print(f"Gene Symbol: {gene_symbol}")
     print("--------------")
     i = i + 1
-    if i == 4: #just to see if it is working... It is working.
-        break
+
+print("disease to gene", i)
+
+#Iterate through disease_to_phenotype elements
+i = 0
+j = 0
+for disorder in disease_to_phenotype.find_all('Disorder'):
+    # Extract information from each Disorder element
+    disease_id = disorder['id']
+    orpha_code = disorder.find('OrphaCode').text
+    disease_name = disorder.find('Name', {'lang': 'en'}).text
+
+    # Extract information from DisorderPhenotypeAssociation element
+    phenotype_association_list = disorder.find('HPODisorderAssociationList')
+
+    i = i + 1
+
+    if phenotype_association_list:
+        for phenotype_association in phenotype_association_list.find_all('HPODisorderAssociation'):
+            # Extract information from HPODisorderAssociation element
+            phenotype_id = phenotype_association.find('HPO').find('HPOId').text
+            phenotype_name = phenotype_association.find('HPO').find('HPOTerm').text
+            frequency = phenotype_association.find('HPOFrequency').find('Name', {'lang': 'en'}).text
+
+            # Print the information
+            print(f"Disease ID: {disease_id}")
+            print(f"Orpha Code: {orpha_code}")
+            print(f"Disease Name: {disease_name}")
+            print(f"Phenotype ID: {phenotype_id}")
+            print(f"Phenotype Name: {phenotype_name}")
+            print(f"Frequency: {frequency}")
+            print("--------------")
+            j = j + 1
+
+print("In disease_to_phenotype:\n")
+print("disease elements: ", i)
+print("# of disorder_to_phenotype relations: ", j)
