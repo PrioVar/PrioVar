6766b401f18b2ad73a90fa42c1498eafefcc6805
HP <alperengozeten@gmail.com>
2024-02-24 12:07:25 +0300

    feat: add weighted_node2vec.py

---
+++ flask/helpers/weighted_node2vec.py
@@ -0,0 +1,63 @@
+import networkx as nx
+import torch
+
+from stellargraph.data import BiasedRandomWalk
+from stellargraph import StellarGraph
+from gensim.models import Word2Vec
+
+WALK_LENGTH = 100
+
+
+def read_tensor_data(edge_index_path, edge_weight_path):
+    edge_index = torch.load(edge_index_path)
+    edge_weight = torch.load(edge_weight_path)
+    G = nx.Graph()
+    edge_set = set()
+    count = 0
+    for i in range(edge_index.size(1)):
+        src = edge_index[0, i].item()
+        dst = edge_index[1, i].item()
+        weight = edge_weight[i].item()
+
+        # if weight is equal to 1, rescale it to 5 x 10^-5
+        if weight == 1:
+            weight = 5 * 10 ** -5
+
+        # Check if the edge already exists in the set
+        if (src, dst) in edge_set:
+            # Update the weight of the existing edge with the larger weight value
+            G[src][dst]["weight"] = max(G[src][dst]["weight"], weight)
+            count += 1
+            if count % 1000 == 0:
+                print(f"source: {src}, destination: {dst}, weight: {weight}")
+        else:
+            # Add the edge to the graph and the set
+            G.add_edge(src, dst, weight=weight)
+            edge_set.add((src, dst))
+    return G
+
+
+# read the tensors data/edge_index.pt and data/edge_weight.pt into a networkx graph
+G = read_tensor_data("../data/edge_index.pt", "../data/edge_weight.pt")
+
+# create a StellarGraph object from the networkx graph
+G = StellarGraph.from_networkx(G)
+rw = BiasedRandomWalk(G)
+
+weighted_walks = rw.run(
+    nodes=G.nodes(),  # root nodes
+    length=WALK_LENGTH,  # maximum length of a random walk
+    n=10,  # number of random walks per root node
+    p=0.5,  # Defines (unormalised) probability, 1/p, of returning to source node
+    q=2.0,  # Defines (unormalised) probability, 1/q, for moving away from source node
+    weighted=True,  # for weighted random walks
+    seed=42,  # random seed fixed for reproducibility
+)
+print("Number of random walks: {}".format(len(weighted_walks)))
+
+weighted_model = Word2Vec(
+    weighted_walks, vector_size=128, window=5, min_count=0, sg=1, workers=1, epochs=1
+)
+
+# save all embeddings into a file
+weighted_model.wv.save_word2vec_format("../data/node_embeddings.txt")
