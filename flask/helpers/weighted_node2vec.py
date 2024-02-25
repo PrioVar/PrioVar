034ab37907f9a978caab38db37f0b0532227b689
HP <alperengozeten@gmail.com>
2024-02-25 21:18:33 +0300

    feat: make vector size part of the file name

--- flask/helpers/weighted_node2vec.py
+++ flask/helpers/weighted_node2vec.py
@@ -47,6 +47,7 @@ WALK_LENGTH = 100  # maximum length of a random walk
 n = 5  # number of random walks per root node
 p = 0.5  # Defines (unormalised) probability, 1/p, of returning to source node
 q = 2  # Defines (unormalised) probability, 1/q, for moving away from source node
+vector_size = 256  # dimension of node embeddings
 
 weighted_walks = rw.run(
     nodes=G.nodes(),  # root nodes
@@ -58,11 +59,11 @@ weighted_walks = rw.run(
 print("Number of random walks: {}".format(len(weighted_walks)))
 
 weighted_model = Word2Vec(
-    weighted_walks, vector_size=128, window=5, min_count=5, sg=1, workers=2, epochs=5
+    weighted_walks, vector_size=vector_size, window=5, min_count=5, sg=1, workers=2, epochs=5
 )
 
 # Save all embeddings into a file manually
-file_path = f"../data/node_embeddings_p_{p}_q_{q}_n_{n}.txt"
+file_path = f"../data/weighted_node2vec_p{p}_q{q}_n{n}_v{vector_size}.txt"
 with open(file_path, "w") as f:
     f.write(f"{len(weighted_model.wv)} {weighted_model.vector_size}\n")
     keys = weighted_model.wv.index_to_key
