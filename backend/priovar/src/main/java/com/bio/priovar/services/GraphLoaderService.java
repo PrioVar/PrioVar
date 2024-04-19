4cca8010371ab1a0770d3250d0eab44aed3ecd05
HP <alperengozeten@gmail.com>
2024-04-19 22:53:03 +0300

    fix: ensure both local and container development

--- backend/priovar/src/main/java/com/bio/priovar/services/GraphLoaderService.java
+++ backend/priovar/src/main/java/com/bio/priovar/services/GraphLoaderService.java
@@ -1,29 +1,33 @@
 package com.bio.priovar.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class GraphLoaderService {
 
+    @Value("${flask.url}")
+    private String flaskUrl;
+
     public String startHPODataLoading() {
         RestTemplate restTemplate = new RestTemplate();
-        String flaskUrl = "http://localhost:5001/load-hpo";
-        String response = restTemplate.getForObject(flaskUrl, String.class);
+        String loadHPOUrl = flaskUrl + "/load-hpo";
+        String response = restTemplate.getForObject(loadHPOUrl, String.class);
         return response;
     }
 
     public String startDiseaseDataLoading() {
         RestTemplate restTemplate = new RestTemplate();
-        String flaskUrl = "http://localhost:5001/load-diseases";
-        String response = restTemplate.getForObject(flaskUrl, String.class);
+        String loadDiseasesUrl = flaskUrl + "/load-diseases";
+        String response = restTemplate.getForObject(loadDiseasesUrl, String.class);
         return response;
     }
 
     public String startGeneDatafromHPOLoading() {
         RestTemplate restTemplate = new RestTemplate();
-        String flaskUrl = "http://localhost:5001/load-genes";
-        String response = restTemplate.getForObject(flaskUrl, String.class);
+        String loadGenesUrl = flaskUrl + "/load-genes";
+        String response = restTemplate.getForObject(loadGenesUrl, String.class);
         return response;
     }
 }
