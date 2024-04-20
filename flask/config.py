bb7e6dff97becfbb21d90556510065f10b11aa51
HP <alperengozeten@gmail.com>
2024-04-20 14:04:22 +0300

    feat: use env variables for flask app

--- flask/config.py
+++ flask/config.py
@@ -1,4 +1,12 @@
-uri = "neo4j://neo4j:7687"
-username = "neo4j"
-password = "priovar."
+import os
+
+# Set default values for local development
+DEFAULT_NEO4J_URI = "neo4j://localhost:7687"
+DEFAULT_NEO4J_USERNAME = "neo4j"
+DEFAULT_NEO4J_PASSWORD = "priovar."
+
+# Use os.getenv to get the environment variable, if it exists, or default to the local development value
+uri = os.getenv('NEO4J_URI', DEFAULT_NEO4J_URI)
+username = os.getenv('NEO4J_USERNAME', DEFAULT_NEO4J_USERNAME)
+password = os.getenv('NEO4J_PASSWORD', DEFAULT_NEO4J_PASSWORD)
 API_KEY = "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"
