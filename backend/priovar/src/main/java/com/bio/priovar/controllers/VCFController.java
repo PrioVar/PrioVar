f096222a1234b6ff35d7c5c6ea0ed311554fb85b
HP <alperengozeten@gmail.com>
2024-04-22 12:24:53 +0300

    fix: upload vcf file

--- backend/priovar/src/main/java/com/bio/priovar/controllers/VCFController.java
+++ backend/priovar/src/main/java/com/bio/priovar/controllers/VCFController.java
@@ -2,9 +2,7 @@ package com.bio.priovar.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import com.bio.priovar.services.VCFService;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -21,12 +19,10 @@ public class VCFController {
     }
 
     @PostMapping("/upload")
-    public ResponseEntity<String> uploadVCF(@RequestParam("vcfFile") String base64File, @RequestParam("clinicianId") Long clinicianId, 
-    @RequestParam("patientName") String patientName, @RequestParam("patientAge") int patientAge, @RequestParam("patientGender") String patientGender) {
+    public ResponseEntity<String> uploadVCF(@RequestParam("vcfFile") String base64File, @RequestParam("clinicianId") Long clinicianId) {
         // Remove the Base64 prefix if present (e.g., "data:image/png;base64,")
         String base64Data = base64File.substring(base64File.indexOf(",") + 1);
-        return vcfService.uploadVCF(base64Data, clinicianId, patientName, patientAge, patientGender);
-
+        return vcfService.uploadVCF(base64Data, clinicianId);
     }
 
 }
