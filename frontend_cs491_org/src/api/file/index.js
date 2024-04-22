71b301ff37acf51baa29171964b9e52ed8dd7973
Erkin Aydın <erkinresearch@gmail.com>
2024-04-22 19:59:19 +0300

    feat: MyPatientsTable has been started, but not finished yet. A new branch will be open soon.

--- frontend_cs491_org/src/api/file/index.js
+++ frontend_cs491_org/src/api/file/index.js
@@ -1,3 +1,3 @@
-export { useFiles, useBedFiles, updateFileNotes, updateFinishInfo, fecthClinicianFiles, fecthMedicalCenterFiles } from './list'
+export { useFiles, useBedFiles, updateFileNotes, updateFinishInfo, fecthClinicianFiles, fecthMedicalCenterFiles, fetchClinicianPatients } from './list'
 export { annotateFile, getPlots } from './annotate'
 export { updateDetails } from './details'
