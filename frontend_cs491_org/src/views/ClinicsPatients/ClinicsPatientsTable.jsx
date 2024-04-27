09a8750222a20035d92b37ad7d7bff3b2d8c2b62
Erkin Aydın <erkinresearch@gmail.com>
2024-04-27 23:29:34 +0300

    feat: delete buttons shall be working properly for My Patients and Clinics Patients pages.

--- frontend_cs491_org/src/views/ClinicsPatients/ClinicsPatientsTable.jsx
+++ frontend_cs491_org/src/views/ClinicsPatients/ClinicsPatientsTable.jsx
@@ -21,7 +21,7 @@ import {
   import { fDateTime } from 'src/utils/formatTime'
   import JobStateStatus from '../common/JobStateStatus'
   import { deleteVcfFile } from '../../api/vcf'
-  import { annotateFile, updateFinishInfo, updateFileNotes, fecthMedicalCenterPatients } from '../../api/file'
+  import { annotateFile, updateFinishInfo, updateFileNotes, fecthMedicalCenterPatients, deletePatient } from '../../api/file'
   import { PATH_DASHBOARD, ROOTS_PrioVar } from '../../routes/paths'
   import axios from '../../utils/axios'
   import { Link as RouterLink } from 'react-router-dom'
@@ -242,7 +242,7 @@ import {
             if (!row) return null
   
             const handleClickConfirm = () => {
-              deleteVcfFile(row.patientId).then(() => {
+              deletePatient(row.patientId).then(() => {
               });
             }
   
