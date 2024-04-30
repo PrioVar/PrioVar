376aa954a3ec24d56db92fd89ce0007bbe368544
HP <alperengozeten@gmail.com>
2024-04-30 19:30:56 +0300

    feat: create the knowledge graph class

--- flask/helpers/knowledge_graph.py
+++ flask/helpers/knowledge_graph.py
@@ -1,29 +1,45 @@
 import os
+from neo4j import GraphDatabase
 from langchain.chains import GraphCypherQAChain
 from langchain_openai import ChatOpenAI
 from langchain_community.graphs import Neo4jGraph
 from config import uri, username, password, API_KEY
 
-os.environ["OPENAI_API_KEY"] = API_KEY
 
-graph = Neo4jGraph(
-    url=uri, username=username, password=password
-)
+class KnowledgeGraphQA:
+    def __init__(self):
+        """
+        Initializes the Neo4j graph connection, the QA chain, and the Neo4j driver.
+        """
+        # Set the OpenAI API key
+        os.environ["OPENAI_API_KEY"] = API_KEY
 
-chain = GraphCypherQAChain.from_llm(
-    graph=graph,
-    cypher_llm=ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0125"),
-    qa_llm=ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0125"),
-    verbose=True,
-    validate_cypher=True,
-)
+        # Initialize the graph
+        self.graph = Neo4jGraph(
+            url=uri, username=username, password=password
+        )
+
+        # Set up the QA chain with specific configurations
+        self.chain = GraphCypherQAChain.from_llm(
+            graph=self.graph,
+            cypher_llm=ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0125"),
+            qa_llm=ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0125"),
+            verbose=True,
+            validate_cypher=True,
+        )
+
+        # Initialize the Neo4j driver
+        self.driver = GraphDatabase.driver(uri, auth=(username, password))
 
 
-def get_answer(question: str):
+def get_answer(question: str) -> str:
     """
-    Get the answer by querying the knowledge graph.
+    Retrieves the answer by querying the knowledge graph using the provided question.
+
+    :param kg_qa: An instance of KnowledgeGraphQA for accessing the graph and chain
     :param question: Question from the user
-    :return: Answer
+    :return: Answer as a string
     """
-    return chain.invoke(" " + question + " ")
-
+    kg_qa = KnowledgeGraphQA()
+    answer = kg_qa.chain.invoke(" " + question + " ")
+    return answer
