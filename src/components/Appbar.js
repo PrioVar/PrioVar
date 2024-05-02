6218691d75ff1030d4710f82dfab8954d48a60d3
HP <alperengozeten@gmail.com>
2024-05-02 21:12:34 +0300

    feat: add react icons

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { FaProjectDiagram, FaUsers, FaFileAlt, FaTools, FaPhone, FaDna } from 'react-icons/fa';
 //import LanguageSelector from './LanguageSelector';
 
 const Appbar = () => {
@@ -30,24 +31,26 @@ const Appbar = () => {
   return (
     <nav style={{ backgroundColor: '#f8f9fa', padding: '12px 0' }}>
       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-        <form action="/" method="GET">
-          <input
-            type="submit"
-            className="navbar-brand"
-            style={{
-              background: 'none',
-              border: 'none',
-              marginLeft: '2rem',
-              padding: 0,
-              cursor: 'pointer',
-              fontSize: '1.5rem',
-              fontWeight: 'bold',
-              transition: 'font-size 0.2s ease',
-              fontFamily: 'CircularStd', // Applying the font-family
-            }}
-            value="PrioVar"
-          />
-        </form>
+        <div style={{ display: 'flex', alignItems: 'center', marginLeft: '2rem' }}>
+          <img src="/PrioVar_logo.png" alt="PrioVar Logo" style={{ height: '40px', marginRight: '10px' }} />
+          <form action="/" method="GET">
+            <input
+              type="submit"
+              className="navbar-brand"
+              style={{
+                background: 'none',
+                border: 'none',
+                padding: 0,
+                cursor: 'pointer',
+                fontSize: '1.5rem',
+                fontWeight: 'bold',
+                transition: 'font-size 0.2s ease',
+                fontFamily: 'CircularStd',
+              }}
+              value="PrioVar"
+            />
+          </form>
+        </div>
         {isMobile ? (
           <button
             style={{ background: 'none', marginRight: '2rem', border: 'none', padding: 0, cursor: 'pointer', fontSize: '1.5rem' }}
@@ -58,12 +61,12 @@ const Appbar = () => {
           </button>
         ) : (
           <div style={{ display: 'flex', justifyContent: 'flex-start', maxWidth: '960px', margin: '0 auto' }}>
-            <NavItem to="/" label="Project" style={{ fontWeight: 'bold', fontFamily: 'CircularStd', fontSize: '1rem' }}/>
-            <NavItem to="/team" label="Team" style={{ fontWeight: 'bold', fontFamily: 'CircularStd', fontSize: '1rem' }}/>
-            <NavItem to="/reports" label="Reports" style={{ fontWeight: 'bold', fontFamily: 'CircularStd', fontSize: '1rem' }}/>
-            <NavItem to="/resources" label="Resources Used" style={{ fontWeight: 'bold', fontFamily: 'CircularStd', fontSize: '1rem' }}/>
-            <NavItem to="/contact-us" label="Contact Us" style={{ fontWeight: 'bold', fontFamily: 'CircularStd', fontSize: '1rem' }}/>
-            <NavItem to="/priovar" label="PrioVar" style={{ fontWeight: 'bold', fontFamily: 'CircularStd', fontSize: '1rem' }}/>
+            <NavItem to="/" label="Project" iconColor="#007BFF" Icon={FaProjectDiagram} style={{ fontWeight: 'bold', fontFamily: 'CircularStd', fontSize: '1rem' }}/>
+            <NavItem to="/team" label="Team" iconColor="#0056B3" Icon={FaUsers} style={{ fontWeight: 'bold', fontFamily: 'CircularStd', fontSize: '1rem'  }}/>
+            <NavItem to="/reports" label="Reports" iconColor="#6C757D" Icon={FaFileAlt} style={{ fontWeight: 'bold', fontFamily: 'CircularStd', fontSize: '1rem'  }}/>
+            <NavItem to="/resources" label="Resources Used" iconColor="#343A40" Icon={FaTools} style={{ fontWeight: 'bold', fontFamily: 'CircularStd', fontSize: '1rem'  }}/>
+            <NavItem to="/contact-us" label="Contact Us" iconColor="#004085" Icon={FaPhone} style={{ fontWeight: 'bold', fontFamily: 'CircularStd', fontSize: '1rem'  }}/>
+            <NavItem to="/priovar" label="PrioVar" iconColor="#003E70" Icon={FaDna} style={{ fontWeight: 'bold', fontFamily: 'CircularStd', fontSize: '1rem'  }}/>
             {
               /* 
               <div style={{ display: 'flex', alignItems: 'center', marginLeft: '0.5rem', fontWeight: 'bold' }}>
@@ -76,12 +79,12 @@ const Appbar = () => {
       </div>
       {isNavDisplayedMobile && (
         <div style={{ display: 'flex', justifyContent: 'flex-end', maxWidth: '960px', margin: '0 auto' }}>
-          <NavItem to="/" label="Project"  style={{ fontWeight: 'bold' }}/>
-            <NavItem to="/team" label="Team"  style={{ fontWeight: 'bold' }}/>
-            <NavItem to="/reports" label="Reports"  style={{ fontWeight: 'bold' }}/>
-            <NavItem to="/resources" label="Resources Used"  style={{ fontWeight: 'bold' }}/>
-            <NavItem to="/contact-us" label="Contact Us"  style={{ fontWeight: 'bold' }}/>
-            <NavItem to="/priovar" label="PrioVar"  style={{ fontWeight: 'bold' }}/>
+            <NavItem to="/" label="Project" iconColor="#007BFF" Icon={FaProjectDiagram} style={{ fontWeight: 'bold', fontFamily: 'CircularStd', fontSize: '1rem' }}/>
+            <NavItem to="/team" label="Team" iconColor="#0056B3" Icon={FaUsers} style={{ fontWeight: 'bold', fontFamily: 'CircularStd', fontSize: '1rem'  }}/>
+            <NavItem to="/reports" label="Reports" iconColor="#6C757D" Icon={FaFileAlt} style={{ fontWeight: 'bold', fontFamily: 'CircularStd', fontSize: '1rem'  }}/>
+            <NavItem to="/resources" label="Resources Used" iconColor="#343A40" Icon={FaTools} style={{ fontWeight: 'bold', fontFamily: 'CircularStd', fontSize: '1rem'  }}/>
+            <NavItem to="/contact-us" label="Contact Us" iconColor="#004085" Icon={FaPhone} style={{ fontWeight: 'bold', fontFamily: 'CircularStd', fontSize: '1rem'  }}/>
+            <NavItem to="/priovar" label="PrioVar" iconColor="#003E70" Icon={FaDna} style={{ fontWeight: 'bold', fontFamily: 'CircularStd', fontSize: '1rem'  }}/>
             {
               /*
                 <NavItem style = {{ className:'ml-auto' }}>
@@ -98,7 +101,7 @@ const Appbar = () => {
   );
 };
 
-const NavItem = ({ to, label , style}) => {
+const NavItem = ({ to, label, style, Icon, iconColor }) => {
   const [isHovered, setIsHovered] = useState(false);
 
   const handleMouseEnter = () => {
@@ -122,6 +125,7 @@ const NavItem = ({ to, label , style}) => {
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
+      {Icon && <Icon style={{ marginRight: '10px', fontSize: '1.5rem', color: iconColor }} />}
       <input
         type="submit"
         style={{ background: 'none', border: 'none', padding: 0, cursor: 'pointer', ...style }}
