4f81d9dfced1af092d9de0cfca47d20f87f744f5
KaanTekTr <kaan@spiky.ai>
2024-05-02 17:57:54 +0300

    feat: add background image to Patient Details page

--- frontend_cs491_org/src/views/PatientDetails/index.js
+++ frontend_cs491_org/src/views/PatientDetails/index.js
@@ -10,7 +10,17 @@ import PatientDetailsTable from './PatientDetailsTable'
 
 const PatientDetails = function () {
   return (
-    <Page title="Patient Details">
+    <Page title="Patient Details" style={{
+        top: 0, 
+        left: 0, 
+        width: '100%', 
+        height: '100vh', 
+        backgroundImage: 'url("/static/new_images/ai_dna_2.png")', 
+        backgroundSize: 'cover', 
+        backgroundPosition: 'center center',
+        backgroundColor: 'rgba(255, 255, 255, 0.9)', // Adds white transparency
+        backgroundBlendMode: 'overlay' // This blends the background color with the image
+      }}>
       <Grid container spacing={5}>
 
         <Grid item xs={12}>
