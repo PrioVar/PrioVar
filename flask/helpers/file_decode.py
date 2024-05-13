342693419b6f58e5e1c96925ce467e4dee1acc90
HP <alperengozeten@gmail.com>
2024-05-13 22:33:45 +0300

    feat: read_file_content

--- flask/helpers/file_decode.py
+++ flask/helpers/file_decode.py
@@ -38,3 +38,21 @@ def read_file_content_and_return_df(vcf_id):
             os.remove(temp.name)
     else:
         return "No file content found"
+
+
+def read_file_content(vcf_id):
+    driver = GraphDatabase.driver(uri, auth=(username, password))
+
+    # get the VCFFile with the given vcf_id, use ID() to access
+    with driver.session() as session:
+        result = session.run(
+            "MATCH (v:VCFFile) WHERE ID(v) = $vcf_id RETURN v", {"vcf_id": vcf_id}
+        )
+        vcf_file = result.single()[0]
+
+    if vcf_file and 'content' in vcf_file:
+        base64_content = vcf_file['content']
+        decoded_content = base64.b64decode(base64_content)
+        return decoded_content
+    else:
+        return "No file content found"
